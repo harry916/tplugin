fb5a8da3798ada4fe498347c25b8b2a7bbc46196
harry916 <harry916@126.com>
2015-05-25 00:48:49 +0800

    return UI display data

--- tplugin-services/src/com/harry/tplugin/jersey/CustomerRestService.java
+++ tplugin-services/src/com/harry/tplugin/jersey/CustomerRestService.java
@@ -1,5 +1,7 @@
 package com.harry.tplugin.jersey;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +9,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
@@ -20,6 +23,7 @@ import com.harry.tplugin.trade.SendAsset;
 import com.harry.tplugin.trade.SplitAsset;
 import com.harry.tplugin.trade.StoreAsset;
 import com.harry.tplugin.util.Cause;
+import com.harry.tplugin.util.JacksonUtils;
 import com.harry.tplugin.util.Logger4j;
 import com.harry.tplugin.util.TaoClientWrapper;
 import com.harry.tplugin.util.TpluginType.TPLUGIN_RETURN_CODE;
@@ -33,9 +37,58 @@ public class CustomerRestService {
     private StoreAsset mStoreAsset = new StoreAsset();
     private SplitAsset mSplitAsset = new SplitAsset();
     private SendAsset mSendAsset = new SendAsset();
+
+    public static class ItemInfo{
+        private String proName;
+        private int orderNum;
+        private int shNum;
+        private int bjNum;
+        private int gzNum;
+        public ItemInfo() {
+            super();
+        }
+        public String getProName() {
+            return proName;
+        }
+        public void setProName(String proName) {
+            this.proName = proName;
+        }
+        public int getOrderNum() {
+            return orderNum;
+        }
+        public void setOrderNum(int orderNum) {
+            this.orderNum = orderNum;
+        }
+        public int getShNum() {
+            return shNum;
+        }
+        public void setShNum(int shNum) {
+            this.shNum = shNum;
+        }
+        public int getBjNum() {
+            return bjNum;
+        }
+        public void setBjNum(int bjNum) {
+            this.bjNum = bjNum;
+        }
+        public int getGzNum() {
+            return gzNum;
+        }
+        public void setGzNum(int gzNum) {
+            this.gzNum = gzNum;
+        }
+        @Override
+        public String toString() {
+            return "ItemInfo [proName=" + proName + ", orderNum=" + orderNum
+                    + ", shNum=" + shNum + ", bjNum=" + bjNum + ", gzNum="
+                    + gzNum + "]";
+        }
+
+    }
     
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/{tid}")
     public String getUIDisplayData(@PathParam("tid") String tid)
     {
@@ -55,9 +108,72 @@ public class CustomerRestService {
         List<StoreDetail> stDetais = mStoreAsset.getStoreDetailList(orderList);
         List<SplitItem> splitItems = mSplitAsset.getSplitList(orderCustomerInfo.get("receiver_state"), orderList, stDetais);
         List<DeliveryFormal> deliveryFormal = mSendAsset.getSendList(orderCustomerInfo.get("receiver_state"), splitItems);
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("tid", tid);
+        resultMap.put("state", orderCustomerInfo.get("receiver_state"));
+        List<ItemInfo> itemList = new ArrayList<CustomerRestService.ItemInfo>();
+        List<Integer> shDeliver = new ArrayList<Integer>();
+        List<Integer> bjDeliver = new ArrayList<Integer>();
+        List<Integer> gzDeliver = new ArrayList<Integer>();
+        List<Integer> beyond = new ArrayList<Integer>();
+        List<Integer> lack = new ArrayList<Integer>();
+        for (int i = 0; i < orderList.size(); i++)
+        {
+            OrderItem oder = orderList.get(i);
+            ItemInfo tItem = new ItemInfo();
+            tItem.setProName(oder.getProName());
+            tItem.setOrderNum((int) oder.getNum());
+            String proId = oder.getProId();
+            for (int j = 0; j < splitItems.size(); j++)
+            {
+                SplitItem splitItem = splitItems.get(j);
+                if (proId.equals(splitItem.getProId()))
+                {
+                    if ("021".equals(splitItem.getStoreId()))
+                        tItem.setShNum(splitItem.getStoreNum());
+                    if ("010".equals(splitItem.getStoreId()))
+                        tItem.setBjNum(splitItem.getStoreNum());
+                    if ("022".equals(splitItem.getStoreId()))
+                        tItem.setGzNum(splitItem.getStoreNum());
+                }
+            }
+            itemList.add(tItem);
+            
+            for (int j = 0; j < deliveryFormal.size(); j++)
+            {
+                DeliveryFormal dFormal = deliveryFormal.get(j);
+                if (!dFormal.getProId().equals(proId))
+                    continue;
+                if ("缺货不发".equals(dFormal.getComment()))
+                {
+                    lack.add(i);
+                }
+                else if ("上海发货".equals(dFormal.getComment()))
+                {
+                    shDeliver.add(i);
+                }
+                else if ("北京发货".equals(dFormal.getComment()))
+                {
+                    bjDeliver.add(i);
+                }
+                else if ("广州发货".equals(dFormal.getComment()))
+                {
+                    gzDeliver.add(i);
+                }
+                else if ("超范围不发".equals(dFormal.getComment()))
+                {
+                    beyond.add(i);
+                }
+            }
+        }
+        resultMap.put("itemInfoList", itemList);
+        resultMap.put("shDelivery", shDeliver);
+        resultMap.put("bjDelivery", bjDeliver);
+        resultMap.put("gzDelivery", gzDeliver);
+        resultMap.put("lack", lack);
+        resultMap.put("beyond", beyond);
         
-        
-        return "";
+        return JacksonUtils.getJsonString(resultMap);
     }
     
     private boolean isTidValid(String tid)
