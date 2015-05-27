841b1861cba48a3ce1d84b9eecd3b3e2d4eab607
harry916 <harry916@126.com>
2015-05-27 17:44:50 +0800

    pass data from index.jsp to backend(login)

--- tplugin-services/src/com/harry/tplugin/jersey/LoginRestService.java
+++ tplugin-services/src/com/harry/tplugin/jersey/LoginRestService.java
@@ -4,59 +4,67 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
 
 import com.harry.tplugin.bean.Login;
 import com.harry.tplugin.service.LoginService;
-import com.harry.tplugin.util.Cause;
 import com.harry.tplugin.util.JacksonUtils;
 import com.harry.tplugin.util.Logger4j;
 import com.harry.tplugin.util.ServiceFactoryBean;
-import com.harry.tplugin.util.TaoClientWrapper;
-import com.harry.tplugin.util.TpluginType.TPLUGIN_RETURN_CODE;
 
-@Path("/login")
-public class LoginRestService {
+@Path("/")
+public class LoginRestService{
 
 	private static final Logger log = Logger4j.getLogger(LoginRestService.class);
 	
 	private static LoginService loginService = ServiceFactoryBean.getLoginService();
 	
+	private final static String ADMIN_POWERCODE = "11111111";
+	
 	@POST
-    @Consumes(MediaType.APPLICATION_JSON) 
-	public String login(String jsonString, @Context HttpServletRequest request){
+	@Path("/login")
+	public String login( @Context HttpServletRequest request,@FormParam("userName") String userName,@FormParam("password") String password){
+		System.out.println("@mawz:  userName:"+userName+"  password:"+password);
 	    Map<String, Object> resultMap = new HashMap<String, Object>();
-        Map<String, Object> dataMap = new HashMap<String, Object>();
         try {
-            @SuppressWarnings("unchecked")
-            Map<String, String> map =  JacksonUtils.objectMapper.readValue(jsonString, Map.class);
-            if(null == map.get("userName") || null == map.get("password")){
-                return Cause.getFailcode(TPLUGIN_RETURN_CODE.TPLUGIN_ERR_ARGUMENT.getValue(), "login", "userName or password can not be null");
+            if (null == userName || null == password){
+            	resultMap.put("ack", "failed");
+            	resultMap.put("cause", "用户名或密码不能为空！");
+            	return JacksonUtils.getJsonString(resultMap);
             }
-            Login logInfo = loginService.findLoginByUserName(map.get("userName"));
+            Login logInfo = loginService.findLoginByUserName(userName);
             if (null == logInfo){
-                return Cause.getFailcode(TPLUGIN_RETURN_CODE.TPLUGIN_ERR_LOGIN_NAME.getValue(), "login", "name not exist");
+            	resultMap.put("ack", "failed");
+            	resultMap.put("cause", "没有此用户！");
+            	return JacksonUtils.getJsonString(resultMap);
             }
-            if (!logInfo.getPassword().equals(map.get("password")))
+            if (!logInfo.getPassword().equals(password))
             {
-                return Cause.getFailcode(TPLUGIN_RETURN_CODE.TPLUGIN_ERR_LOGIN_PASSWORD.getValue(), "login", "password failed");
+            	resultMap.put("ack", "failed");
+            	resultMap.put("cause", "密码错误！");
+            	return JacksonUtils.getJsonString(resultMap);
             }
             if (!isIpAddrPermit(getIpAddr(request), logInfo))
             {
-                return Cause.getFailcode(TPLUGIN_RETURN_CODE.TPLUGIN_ERR_LOGIN_IP.getValue(), "login", "IP not permitted");
+            	resultMap.put("ack", "failed");
+            	resultMap.put("cause", "您的IP地址没有登陆权限！");
+            	return JacksonUtils.getJsonString(resultMap);
             }
-           
-            resultMap.put("ack", "success");
-            dataMap.put("userName", logInfo.getUserName());
-            dataMap.put("token", null);
-            resultMap.put("data", dataMap);
-            TaoClientWrapper.getInstance();
+            if (ADMIN_POWERCODE.equals(logInfo.getPowerCode()))
+            {
+            	resultMap.put("ack", "success");
+            	return JacksonUtils.getJsonString(resultMap);
+            }
+            else
+            {
+            	resultMap.put("ack", "customer");
+            	return JacksonUtils.getJsonString(resultMap);
+            }
         } catch (Exception e) {
             e.printStackTrace();
             resultMap.put("ack", "failed");
